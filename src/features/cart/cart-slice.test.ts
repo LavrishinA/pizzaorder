cc924624972302e7359e2737b528e0becd9f49ac
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-01-12 01:17:50 +0300

    feat: add deleteItem, increase and decrease quantity item, clearCart reducers, and write test for this reducers

--- src/features/cart/cart-slice.test.ts
+++ src/features/cart/cart-slice.test.ts
@@ -1,3 +1,4 @@
+import { beforeEach, expect, test } from "vitest"
 import { CartItem } from "../../api/pizzaApi.ts"
 import { cartActions, cartReducer } from "./cart-slice.tsx"
 
@@ -9,7 +10,7 @@ beforeEach(() => {
             {
                 pizzaId: 1,
                 name: "Margherita",
-                quantity: 1,
+                quantity: 2,
                 unitPrice: 12,
                 totalPrice: 12,
             },
@@ -20,7 +21,7 @@ beforeEach(() => {
 test("new item should be added into cart", () => {
     const reducedState = cartReducer(
         initialState,
-        cartActions.addToCart({
+        cartActions.addItem({
             pizzaId: 2,
             name: "Capricciosa",
             unitPrice: 14,
@@ -29,4 +30,28 @@ test("new item should be added into cart", () => {
 
     expect(reducedState.cart.length).toBe(2)
     expect(reducedState.cart[1].name).toBe("Capricciosa")
+})
+
+test("item by id should be deleted", () => {
+    const reducedState = cartReducer(initialState, cartActions.deleteItem(1))
+
+    expect(reducedState.cart.length).toBe(0)
+})
+
+test("item quantity should be increased", () => {
+    const reducedState = cartReducer(initialState, cartActions.increaseQuantity(1))
+
+    expect(reducedState.cart[0].quantity).toBe(3)
+})
+
+test("item quantity should be decreased", () => {
+    const reducedState = cartReducer(initialState, cartActions.decreaseQuantity(1))
+
+    expect(reducedState.cart[0].quantity).toBe(1)
+})
+
+test("cart should be cleared", () => {
+    const reducedState = cartReducer(initialState, cartActions.clearCart())
+
+    expect(reducedState.cart.length).toBe(0)
 })
--- src/features/cart/cart-slice.tsx
+++ src/features/cart/cart-slice.tsx
@@ -9,13 +9,28 @@ const slice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        addToCart: {
+        addItem: {
             reducer(state, action: PayloadAction<CartItem>) {
                 state.cart.push(action.payload)
             },
             prepare(payload: { pizzaId: number; name: string; unitPrice: number }) {
                 return { payload: { ...payload, quantity: 1, totalPrice: payload.unitPrice } }
             },
+        },
+        deleteItem: (state, action: PayloadAction<number>) => {
+            const index = state.cart.findIndex((p) => p.pizzaId === action.payload)
+            if (index !== -1) state.cart.splice(index, 1)
+        },
+        increaseQuantity: (state, action: PayloadAction<number>) => {
+            const index = state.cart.findIndex((p) => p.pizzaId === action.payload)
+            if (index !== -1) state.cart[index].quantity += 1
+        },
+        decreaseQuantity: (state, action: PayloadAction<number>) => {
+            const index = state.cart.findIndex((p) => p.pizzaId === action.payload)
+            if (index !== -1) state.cart[index].quantity -= 1
+        },
+        clearCart: (state) => {
+            state.cart = []
         },
     },
 })
